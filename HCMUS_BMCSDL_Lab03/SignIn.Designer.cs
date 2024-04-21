74f07c24b9b0a85b736515551a76e49b25f1d839
Mai Hanh <nguyentrangmaihanh@gmail.com>
2024-04-21 21:15:13 +0700

    update Sign in

--- HCMUS_BMCSDL_Lab03/SignIn.Designer.cs
+++ HCMUS_BMCSDL_Lab03/SignIn.Designer.cs
@@ -28,40 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelForm = new System.Windows.Forms.Label();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textboxUsername = new System.Windows.Forms.TextBox();
+            this.textboxPassword = new System.Windows.Forms.TextBox();
             this.btn_signin = new System.Windows.Forms.Button();
+            this.btn_exitSignIn = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // labelForm
+            // 
+            this.labelForm.AutoSize = false;
+            this.labelForm.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelForm.Font = new System.Drawing.Font("Arial Black", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelForm.ForeColor = System.Drawing.Color.White;
+            this.labelForm.Location = new System.Drawing.Point(143, 70);
+            this.labelForm.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelForm.Name = "labelForm";
+            this.labelForm.Size = new System.Drawing.Size(160, 45);
+            this.labelForm.TabIndex = 0;
+            this.labelForm.Text = "SIGN IN";
+            this.labelForm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = false;
+            this.labelUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUsername.ForeColor = System.Drawing.Color.White;
+            this.labelUsername.Location = new System.Drawing.Point(45, 143);
+            this.labelUsername.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(87, 20);
+            this.labelUsername.TabIndex = 1;
+            this.labelUsername.Text = "Username:";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = false;
+            this.labelPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPassword.ForeColor = System.Drawing.Color.White;
+            this.labelPassword.Location = new System.Drawing.Point(50, 193);
+            this.labelPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(82, 20);
+            this.labelPassword.TabIndex = 2;
+            this.labelPassword.Text = "Password:";
+            // 
+            // textboxUsername
+            // 
+            this.textboxUsername.AutoSize = false;
+            this.textboxUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textboxUsername.ForeColor = System.Drawing.Color.Black;
+            this.textboxUsername.Location = new System.Drawing.Point(135, 140);
+            this.textboxUsername.Margin = new System.Windows.Forms.Padding(2);
+            this.textboxUsername.Name = "textboxUsername";
+            this.textboxUsername.Size = new System.Drawing.Size(225, 30);
+            this.textboxUsername.TabIndex = 2;
+            this.textboxUsername.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textboxPassword
+            // 
+            this.textboxPassword.AutoSize = false;
+            this.textboxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textboxPassword.ForeColor = System.Drawing.Color.Black;
+            this.textboxPassword.Location = new System.Drawing.Point(135, 190);
+            this.textboxPassword.Margin = new System.Windows.Forms.Padding(2);
+            this.textboxPassword.Name = "textboxPassword";
+            this.textboxPassword.PasswordChar = '*';
+            this.textboxPassword.Size = new System.Drawing.Size(225, 30);
+            this.textboxPassword.TabIndex = 3;
+            this.textboxPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // btn_signin
             // 
-            this.btn_signin.BackColor = System.Drawing.Color.Aqua;
-            this.btn_signin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_signin.AutoSize = false;
+            this.btn_signin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_signin.ForeColor = System.Drawing.Color.Black;
-            this.btn_signin.Location = new System.Drawing.Point(206, 353);
+            this.btn_signin.Location = new System.Drawing.Point(135, 235);
+            this.btn_signin.Margin = new System.Windows.Forms.Padding(2);
             this.btn_signin.Name = "btn_signin";
-            this.btn_signin.Size = new System.Drawing.Size(112, 47);
-            this.btn_signin.TabIndex = 26;
-            this.btn_signin.Text = "Sign in";
-            this.btn_signin.UseVisualStyleBackColor = false;
+            this.btn_signin.Size = new System.Drawing.Size(90, 35);
+            this.btn_signin.TabIndex = 4;
+            this.btn_signin.Text = "Sign In";
+            this.btn_signin.UseVisualStyleBackColor = true;
             this.btn_signin.Click += new System.EventHandler(this.btn_signin_Click);
-            this.btn_signin.MouseLeave += new System.EventHandler(this.btn_signin_MouseLeave);
-            this.btn_signin.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btn_signin_MouseMove);
+            // 
+            // btn_exitSignIn
+            // 
+            this.btn_exitSignIn.AutoSize = false;
+            this.btn_exitSignIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_exitSignIn.ForeColor = System.Drawing.Color.Black;
+            this.btn_exitSignIn.Location = new System.Drawing.Point(275, 235);
+            this.btn_exitSignIn.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_exitSignIn.Name = "btn_exitSignIn";
+            this.btn_exitSignIn.Size = new System.Drawing.Size(80, 35);
+            this.btn_exitSignIn.TabIndex = 5;
+            this.btn_exitSignIn.Text = "Exit";
+            this.btn_exitSignIn.UseVisualStyleBackColor = true;
+            this.btn_exitSignIn.Click += new System.EventHandler(this.btn_exitSignIn_Click);
             // 
             // SignIn
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(574, 450);
+            this.ClientSize = new System.Drawing.Size(430, 366);
+            this.Controls.Add(this.btn_exitSignIn);
             this.Controls.Add(this.btn_signin);
+            this.Controls.Add(this.textboxUsername);
+            this.Controls.Add(this.textboxPassword);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelForm);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "SignIn";
-            this.Text = "Sign in";
+            this.Text = "Sign In";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.Label labelForm;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textboxUsername;
+        private System.Windows.Forms.TextBox textboxPassword;
         private System.Windows.Forms.Button btn_signin;
+        private System.Windows.Forms.Button btn_exitSignIn;
     }
 }
-
--- HCMUS_BMCSDL_Lab03/SignIn.cs
+++ HCMUS_BMCSDL_Lab03/SignIn.cs
@@ -5,14 +5,18 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace HCMUS_BMCSDL_Lab03
 {
+
     public partial class SignIn : Form
     {
+        private const string connectionString = "Data Source=your_server;Initial Catalog=your_database;Integrated Security=True";
+
         public bool isLogin = false;
         SqlConnection conn;
         public SignIn(SqlConnection conn)
@@ -29,22 +33,98 @@ namespace HCMUS_BMCSDL_Lab03
 
         private void btn_signin_Click(object sender, EventArgs e)
         {
-            goToHome();
+            string username = textboxUsername.Text;
+            string password = textboxPassword.Text;
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please enter username and password!");
+                return;
+            }
+            if (IsLoginValid(username, password))
+            {
+                MessageBox.Show("Sign In successfully!");
+                goToHome();
+            }
+            else
+            {
+                MessageBox.Show("Username or Password is incorrect.");
+            }
         }
+
+
+        private bool IsLoginValid(string username, string password)
+        {
+            using (SqlConnection connection = this.conn)
+            {
+                // Kiểm tra trong bảng NHANVIEN
+                string queryNV = "SELECT MATKHAU FROM NHANVIEN WHERE MANV = @MANV";
+                using (SqlCommand command = new SqlCommand(queryNV, connection))
+                {
+                    command.Parameters.AddWithValue("@MANV", username);
+                    connection.Open();
+                    var hashedPassword = (byte[])command.ExecuteScalar();
+                    if (hashedPassword != null)
+                    {
+                        byte[] inputPasswordHash = SHA1.Create().ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                        if (hashedPassword.SequenceEqual(inputPasswordHash))
+                            return true;
+                    }
+                }
+
+                string querySV = "SELECT MATKHAU FROM SINHVIEN WHERE MASV = @MASV";
+                using (SqlCommand command = new SqlCommand(querySV, connection))
+                {
+                    command.Parameters.AddWithValue("@MASV", username);
+                    var hashedPassword = (byte[])command.ExecuteScalar();
+                    if (hashedPassword != null)
+                    {
+                        byte[] inputPasswordHash = SHA1.Create().ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                        if (hashedPassword.SequenceEqual(inputPasswordHash))
+                            return true;
+                    }
+                }
+            }
+
+            // Nếu không tìm thấy trong cả hai bảng, hoặc mật khẩu không khớp, trả về false
+            return false;
+        }
+
 
         private void btn_signin_MouseHover(object sender, EventArgs e)
         {
-            btn_signin.BackColor = Color.Yellow;
+            btn_signin.BackColor = Color.LightGray;
         }
 
         private void btn_signin_MouseLeave(object sender, EventArgs e)
         {
-            btn_signin.BackColor = Color.Aqua;
+            btn_signin.BackColor = Color.White;
         }
 
         private void btn_signin_MouseMove(object sender, MouseEventArgs e)
         {
-            btn_signin.BackColor = Color.Yellow;
+            btn_signin.BackColor = Color.LightGray;
+        }
+
+        private void btn_exitSignIn_MouseHover(object sender, EventArgs e)
+        {
+            btn_exitSignIn.BackColor = Color.LightGray;
+        }
+
+        private void btn_exitSignIn_MouseMove(object sender, EventArgs e)
+        {
+            btn_exitSignIn.BackColor = Color.LightGray;
+        }
+
+        private void btn_exitSignIn_MouseLeave(object sender, EventArgs e)
+        {
+            btn_exitSignIn.BackColor = Color.White;
+        }
+        private void btn_exitSignIn_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
